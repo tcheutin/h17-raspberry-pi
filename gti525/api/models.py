fc7cbf1626ad4bd05afd24135ba7663b5f6979ef
Thierry Cheutin <cheutin.thierry@gmail.com>
2017-02-18 17:03:07 -0500

    Modification to the Mobile and Termianl Object
        - Update to the Mobile and Terminal fixture
        - Fix in the migration files

--- gti525/api/models.py
+++ gti525/api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+# Create your models here
 class Terminal(models.Model):
-    ''' BD local to a PI '''
+    ''' Object representation of a Raspberry PI terminal '''
     CONNECTED = 'Connected'
     NON_RESPONSIVE = 'Non-Responsive'
     CONNECTION_STATUS = ((CONNECTED, 'Connected'),
@@ -12,20 +13,32 @@ class Terminal(models.Model):
     status  = models.CharField(max_length=30,
                                choices=CONNECTION_STATUS,
                                default=CONNECTED)
-    macAddress = models.CharField(max_length=30)
+    ipAddress = models.CharField(max_length=30, null=True)
 
 class Mobile(models.Model):
-    ''' BD local to a PI '''
+    ''' Object representation of a mobile device '''
+    CONNECTED = 'Connected'
+    NON_RESPONSIVE = 'Non-Responsive'
+    CONNECTION_STATUS = ((CONNECTED, 'Connected'),
+                         (NON_RESPONSIVE, 'Non-Responsive'))
+
     id = models.AutoField(primary_key=True)
     token = models.CharField(max_length=30)
-    status = models.CharField(max_length=30)
+    ipAddress = models.CharField(max_length=30, null=True)
+    status = models.CharField(max_length=30,
+                              choices=CONNECTION_STATUS,
+                              default=CONNECTED)
+    loginTime = models.DateTimeField(default=date.today)
+    logoutTime = models.DateTimeField(null=True)
 
 class Ticket(models.Model):
-    ''' This DB will be shared between the PI '''
+    ''' Object representation of a event ticket '''
     VALIDATED = 'Validated'
     NON_VALIDATED = 'Non-Validated'
+    IN_PROGRESS = 'In Progress'
     TICKET_STATUS = ((VALIDATED, 'Validated'),
-                     (NON_VALIDATED, 'Non-validated'))
+                     (NON_VALIDATED, 'Non-Validated'),
+                     (IN_PROGRESS, 'In Progress'))
 
     ticketHash = models.CharField(max_length=30, unique=True, default=None)
     status = models.CharField(max_length=30,
@@ -33,3 +46,9 @@ class Ticket(models.Model):
                               default=NON_VALIDATED)
     validationTime = models.DateTimeField()
     validationTerminal = models.ForeignKey(Terminal, null=True)
+
+class MobileCommLog(models.Model):
+    ''' Only used to store the log of ours validations attempts '''
+    mobileId = models.ForeignKey(Mobile)
+    ticketId = models.ForeignKey(Ticket)
+    httpResponse = models.IntegerField()
