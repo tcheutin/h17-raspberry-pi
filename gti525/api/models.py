8e66e58d00a453b1f8ab65798d754fd099f06070
Thierry Cheutin <cheutin.thierry@gmail.com>
2017-02-17 19:53:37 -0500

    Add comment for the DB usage between the PI

--- gti525/api/models.py
+++ gti525/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Terminal(models.Model):
+    ''' BD local to a PI '''
     CONNECTED = 'Connected'
     NON_RESPONSIVE = 'Non-Responsive'
     CONNECTION_STATUS = ((CONNECTED, 'Connected'),
@@ -14,11 +15,13 @@ class Terminal(models.Model):
     macAddress = models.CharField(max_length=30)
 
 class Mobile(models.Model):
+    ''' BD local to a PI '''
     id = models.AutoField(primary_key=True)
     token = models.CharField(max_length=30)
     status = models.CharField(max_length=30)
 
 class Ticket(models.Model):
+    ''' This DB will be shared between the PI '''
     VALIDATED = 'Validated'
     NON_VALIDATED = 'Non-Validated'
     TICKET_STATUS = ((VALIDATED, 'Validated'),
